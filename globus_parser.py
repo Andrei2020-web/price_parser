4381db121718afb3fbf6f6087b3bf2742b2d1976
Asus <asus@mail.ru>
2024-05-02 09:02:32 +0300

    Рефакторинг globus_parser.py.

--- globus_parser.py
+++ globus_parser.py
@@ -22,9 +22,6 @@ async def get_promotions_in_stores():
 
     for store_id in stores_id:
         promos = []
-        title_promo = ''
-        promo_discount_label = ''
-        date_promo = ''
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -49,27 +46,10 @@ async def get_promotions_in_stores():
                 actions_in_globus = soup.find('div', id='actions-in-globus').find(
                     'div', class_='js-carousel-adaptive').find_all('div', class_='block')
                 for promo in actions_in_globus:
-                    try:
-                        title_promo = 'Акция: ' + promo.find('a', class_='name').getText(
-                            strip=True).replace('\u00A0', '') + '.'
-                    except:
-                        title_promo = 'Заголовок акции не найден.'
-                    try:
-                        date_promo = promo.find('span',
-                                                class_='time').getText(strip=True).replace(
-                            '\u00A0', '')
-                    except:
-                        date_promo = 'Срок акции не найден.'
-                    try:
-                        promo_discount_label = promo.find('span',
-                                                          class_='short-desc').getText(
-                            strip=True).replace('\u00A0', '')
-                    except:
-                        promo_discount_label = 'Процент скидки не найден.'
-                    try:
-                        promo_url = promo.find('a', class_='name').attr['href']
-                    except:
-                        promo_url = ''
+                    title_promo = await _get_title_promo(promo)
+                    date_promo = await _get_date_promo(promo)
+                    promo_discount_label = await _get_promo_discount_label(promo)
+                    promo_url = await _get_promo_url(promo)
                     print(
                         f'[+] Акция {title_promo}. {promo_discount_label} c {date_promo}')
                     promos.append(
@@ -87,6 +67,43 @@ async def get_promotions_in_stores():
     return find_promotions_in_stores
 
 
+async def _get_promo_url(promo):
+    try:
+        promo_url = promo.find('a', class_='name').attr['href']
+    except:
+        promo_url = ''
+    return promo_url
+
+
+async def _get_promo_discount_label(promo):
+    try:
+        promo_discount_label = promo.find('span',
+                                          class_='short-desc').getText(
+            strip=True).replace('\u00A0', '')
+    except:
+        promo_discount_label = 'Процент скидки не найден.'
+    return promo_discount_label
+
+
+async def _get_date_promo(promo):
+    try:
+        date_promo = promo.find('span',
+                                class_='time').getText(strip=True).replace(
+            '\u00A0', '')
+    except:
+        date_promo = 'Срок акции не найден.'
+    return date_promo
+
+
+async def _get_title_promo(promo):
+    try:
+        title_promo = 'Акция: ' + promo.find('a', class_='name').getText(
+            strip=True).replace('\u00A0', '') + '.'
+    except:
+        title_promo = 'Заголовок акции не найден.'
+    return title_promo
+
+
 async def get_products():
     find_products_in_stores = {}
 
@@ -98,14 +115,6 @@ async def get_products():
             'Cookie': f'globus_hyper_id={store_id[0]}; globus_hyper_name={store_id[1]}; globus_hyper_show_select=1'
         }
         for product_uri in products_uri:
-            title_sku = ''
-            shop_addr = ''
-            price_regular = ''
-            price_primary = ''
-            discount = ''
-            enough = ''
-            duration_discont = ''
-
             # Кол-во повторов, чтобы получить html документ с товаром
             for i in range(main.number_of_attempts):
                 # Пауза между запросами, помогает при блокировке запросов сайтом
@@ -121,74 +130,19 @@ async def get_products():
                     await asyncio.sleep(main.pause_between_attempts_to_get_html)
 
             if html:
+                enough = ''
                 soup = BeautifulSoup(html, 'lxml')
-                try:
-                    title_sku = soup.find('h1',
-                                          class_='catalog-detail__title-h1').getText(
-                        strip=True).replace('\u00A0', '')
-                    print(f'[+] Товар {title_sku}')
-                except:
-                    title_sku = 'Не удалось прочитать наименование товара'
-                    print(
-                        f"Не удалось прочитать наименование товара {product_uri} в магазине лобус {store_id[0]}")
-                try:
-                    shop_addr = soup.find('div',
-                                          class_='header__mobile-giper_title js-header__mobile-change-city-popup').get_text(
-                        strip=True)
-                    print(f'  -- В магазине глобус {shop_addr}:')
-                except:
-                    shop_addr = 'Не удалось прочитать адрес магазина'
-                    print(
-                        f"Не удалось прочитать адрес для товара {product_uri} в магазине {store_id[0]}")
-                try:
-                    price_regular = soup.find('span',
-                                              class_='catalog-detail__item-price-old-main').get_text(
-                        strip=True).replace('\u00A0', '')
-                    print(f'  -- Обычная цена {price_regular}:')
-                except:
-                    price_regular = ''
-                    print(
-                        f"Не удалось прочитать обычную цену для товара {product_uri} в магазине {store_id[0]}")
-                try:
-                    price_primary = soup.find('span',
-                                              class_='catalog-detail__item-price-actual-main').get_text(
-                        strip=True).replace('\u00A0', '')
-                    print(
-                        f'  -- Цена по карте: {price_primary}')
-                except:
-                    price_primary = ''
-                    print(
-                        f"Не удалось прочитать цену со скидкой для товара {product_uri} в магазине {store_id[0]}")
-                try:
-                    discount = soup.find('span', class_='sale--orang-mt').get_text(
-                        strip=True).replace('\u00A0', '')
-                    print(
-                        f'  -- Скидка {discount}')
-                except:
-                    print(
-                        f"Не удалось прочитать скидку для товара {product_uri} в магазине {store_id[0]}")
-                try:
-                    duration_discont = soup.find('div',
-                                                 class_='catalog-detail__header-content').find(
-                        'div', class_='catalog-detail__header-desc-small').get_text(
-                        strip=True).replace('\u00A0', '')
-                    title_sku += f'\n{duration_discont}'
-                except:
-                    print(
-                        f"Не удалось прочитать срок скидки {product_uri} в магазине {store_id[0]}")
-                try:
-                    actual_price_date = soup.find('div',
-                                                  class_='catalog-detail__additional-text').get_text(
-                        strip=True).replace('\u00A0', '')
-                    actual_price_date_index_begin = actual_price_date.index(
-                        'Цены действительны')
-                    actual_price_date_substring = actual_price_date[
-                                                  actual_price_date_index_begin:actual_price_date_index_begin + 67]
+                title_sku = await _get_title_sku(product_uri, soup, store_id)
+                shop_addr = await _get_shop_addr(product_uri, soup, store_id)
+                price_regular = await _get_price_regular(product_uri, soup,
+                                                         store_id)
+                price_primary = await _get_price_primary(product_uri, soup,
+                                                         store_id)
+                discount = await _get_discount(product_uri, soup, store_id)
+                title_sku = await _get_duration_discount(product_uri, soup, store_id, title_sku)
+                actual_price_date_substring = await _get_actual_price_date(soup, store_id)
+                if actual_price_date_substring:
                     shop_addr += f'\n({actual_price_date_substring})'
-                except:
-                    actual_price_date = ''
-                    print(
-                        f"Не удалось прочитать на какую дату цены действительны в магазине {store_id[0]}")
 
                 find_products_in_store.append(
                     {
@@ -203,3 +157,99 @@ async def get_products():
                 )
         find_products_in_stores[store_id[0]] = find_products_in_store
     return find_products_in_stores
+
+
+async def _get_actual_price_date(soup, store_id):
+    try:
+        actual_price_date = soup.find('div',
+                                      class_='catalog-detail__additional-text').get_text(
+            strip=True).replace('\u00A0', '')
+        actual_price_date_index_begin = actual_price_date.index(
+            'Цены действительны')
+        actual_price_date_substring = actual_price_date[
+                                      actual_price_date_index_begin:actual_price_date_index_begin + 67]
+    except:
+        actual_price_date_substring = ''
+        print(
+            f"Не удалось прочитать на какую дату цены действительны в магазине {store_id[0]}")
+    return actual_price_date_substring
+
+
+async def _get_duration_discount(product_uri, soup, store_id, title_sku):
+    try:
+        duration_discount = soup.find('div',
+                                      class_='catalog-detail__header-content').find(
+            'div', class_='catalog-detail__header-desc-small').get_text(
+            strip=True).replace('\u00A0', '')
+        title_sku += f'\n{duration_discount}'
+    except:
+        duration_discount = ''
+        print(
+            f"Не удалось прочитать срок скидки {product_uri} в магазине {store_id[0]}")
+    return title_sku
+
+
+async def _get_discount(product_uri, soup, store_id):
+    try:
+        discount = soup.find('span', class_='sale--orang-mt').get_text(
+            strip=True).replace('\u00A0', '')
+        print(
+            f'  -- Скидка {discount}')
+    except:
+        discount = ''
+        print(
+            f"Не удалось прочитать скидку для товара {product_uri} в магазине {store_id[0]}")
+    return discount
+
+
+async def _get_price_primary(product_uri, soup, store_id):
+    try:
+        price_primary = soup.find('span',
+                                  class_='catalog-detail__item-price-actual-main').get_text(
+            strip=True).replace('\u00A0', '')
+        print(
+            f'  -- Цена по карте: {price_primary}')
+    except:
+        price_primary = ''
+        print(
+            f"Не удалось прочитать цену со скидкой для товара {product_uri} в магазине {store_id[0]}")
+    return price_primary
+
+
+async def _get_price_regular(product_uri, soup, store_id):
+    try:
+        price_regular = soup.find('span',
+                                  class_='catalog-detail__item-price-old-main').get_text(
+            strip=True).replace('\u00A0', '')
+        print(f'  -- Обычная цена {price_regular}:')
+    except:
+        price_regular = ''
+        print(
+            f"Не удалось прочитать обычную цену для товара {product_uri} в магазине {store_id[0]}")
+    return price_regular
+
+
+async def _get_shop_addr(product_uri, soup, store_id):
+    try:
+        shop_addr = soup.find('div',
+                              class_='header__mobile-giper_title js-header__mobile-change-city-popup').get_text(
+            strip=True)
+        print(f'  -- В магазине глобус {shop_addr}:')
+    except:
+        shop_addr = 'Не удалось прочитать адрес магазина'
+        print(
+            f"Не удалось прочитать адрес для товара {product_uri} в магазине {store_id[0]}")
+    return shop_addr
+
+
+async def _get_title_sku(product_uri, soup, store_id):
+    try:
+        title_sku = soup.find('h1',
+                              class_='catalog-detail__title-h1').getText(
+            strip=True).replace('\u00A0', '')
+        print(f'[+] Товар {title_sku}')
+    except:
+        title_sku = 'Не удалось прочитать наименование товара'
+        print(
+            f"Не удалось прочитать наименование товара {product_uri} в магазине лобус {store_id[0]}")
+    return title_sku
