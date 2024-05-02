4adde9a532344250b448875b536eaefac63f5f48
Asus <asus@mail.ru>
2024-05-02 08:37:09 +0300

    Рефакторинг perekrestok_parser.py.

--- perekrestok_parser.py
+++ perekrestok_parser.py
@@ -20,14 +20,6 @@ async def get_products():
     products = []
     products_in_store = {'Перекрёсток': products}
     for product_uri in products_uri:
-        title_sku = ''
-        promotion = ''
-        shop_addr = ''
-        price_regular = ''
-        price_primary = ''
-        discount = ''
-        enough = ''
-
         # Кол-во повторов, чтобы получить html документ с товаром
         for i in range(main.number_of_attempts):
             # Пауза между запросами, помогает при блокировке запросов сайтом
@@ -42,54 +34,16 @@ async def get_products():
                 # Пауза между попытками
                 await asyncio.sleep(main.number_of_attempts)
         if html:
-            soup = BeautifulSoup(html, 'lxml')
+            enough = ''
             shop_addr = 'Перекрёсток'
-            try:
-                title_sku = soup.find('h1',
-                                      class_='sc-fubCzh ibFUIH product__title').getText(
-                    strip=True).replace('\u00A0', '')
-                print(f'[+] Товар {title_sku}')
-            except:
-                title_sku = 'Не удалось прочитать наименование товара'
-                print(
-                    f"Не удалось прочитать наименование товара {product_uri} в магазине перекрёсток")
-            try:
-                promotion = soup.find('div', class_='sc-kYrlTI kRDKSf').find(
-                    'span').get_text(strip=True).replace('\u00A0', '')
-                print(f'  -- акция {promotion}')
-            except:
-                promotion = ''
-                print(
-                    f"Не удалось прочитать акцию товара {product_uri} в магазине перекрёсток")
+            soup = BeautifulSoup(html, 'lxml')
+            title_sku = await _get_title_sku(product_uri, soup)
+            promotion = await _get_promotion(product_uri, soup)
             if promotion:
                 title_sku += f' АКЦИЯ {promotion}'
-            try:
-                price_wrapper = soup.find('div', class_='Flex-brknwi-0 jRbnzW')
-                price_old_wrapper = price_wrapper.find('div', class_='price-old-wrapper')
-                price_regular = price_old_wrapper.find('div',
-                                                       class_='price-old').contents[
-                    -1].strip().replace('\u00A0', '').replace('₽', '')
-                print(f'  -- Обычная цена {price_regular}:')
-            except:
-                print(
-                    f"Не удалось прочитать обычную цену для товара {product_uri} в магазине перекрёсток")
-            try:
-                price_wrapper = soup.find('div', class_='Flex-brknwi-0 jRbnzW')
-                price_primary = price_wrapper.find(
-                    'div', class_='price-new').contents[-1].strip().replace('\u00A0', '').replace(
-                    '₽', '')
-                print(f'  -- Цена по карте: {price_primary}')
-
-            except:
-                print(
-                    f"Не удалось прочитать цену со скидкой для товара {product_uri} в магазине перекрёсток")
-            try:
-                discount = soup.find('div', class_='sc-jGVbWl NgirP').find(
-                    'span').get_text(strip=True).replace('\u00A0', '')
-                print(f'  -- Скидка {discount}')
-            except:
-                print(
-                    f"Не удалось прочитать скидку для товара {product_uri} в магазине перекрёсток")
+            price_regular = await _get_price_regular(product_uri, soup)
+            price_primary = await _get_price_primary(product_uri, soup)
+            discount = await _get_discount(product_uri, soup)
 
             products.append({'title_sku': title_sku,
                              'shop_addr': shop_addr,
@@ -100,3 +54,69 @@ async def get_products():
                              'url': url + product_uri})
 
     return products_in_store
+
+
+async def _get_discount(product_uri, soup):
+    try:
+        discount = soup.find('div', class_='sc-jGVbWl NgirP').find(
+            'span').get_text(strip=True).replace('\u00A0', '')
+        print(f'  -- Скидка {discount}')
+    except:
+        discount = ''
+        print(
+            f"Не удалось прочитать скидку для товара {product_uri} в магазине перекрёсток")
+    return discount
+
+
+async def _get_price_primary(product_uri, soup):
+    try:
+        price_wrapper = soup.find('div', class_='Flex-brknwi-0 jRbnzW')
+        price_primary = price_wrapper.find(
+            'div', class_='price-new').contents[-1].strip().replace('\u00A0', '').replace(
+            '₽', '')
+        print(f'  -- Цена по карте: {price_primary}')
+    except:
+        price_primary = ''
+        print(
+            f"Не удалось прочитать цену со скидкой для товара {product_uri} в магазине перекрёсток")
+    return price_primary
+
+
+async def _get_price_regular(product_uri, soup):
+    try:
+        price_wrapper = soup.find('div', class_='Flex-brknwi-0 jRbnzW')
+        price_old_wrapper = price_wrapper.find('div', class_='price-old-wrapper')
+        price_regular = price_old_wrapper.find('div',
+                                               class_='price-old').contents[
+            -1].strip().replace('\u00A0', '').replace('₽', '')
+        print(f'  -- Обычная цена {price_regular}:')
+    except:
+        price_regular = ''
+        print(
+            f"Не удалось прочитать обычную цену для товара {product_uri} в магазине перекрёсток")
+    return price_regular
+
+
+async def _get_promotion(product_uri, soup):
+    try:
+        promotion = soup.find('div', class_='sc-kYrlTI kRDKSf').find(
+            'span').get_text(strip=True).replace('\u00A0', '')
+        print(f'  -- акция {promotion}')
+    except:
+        promotion = ''
+        print(
+            f"Не удалось прочитать акцию товара {product_uri} в магазине перекрёсток")
+    return promotion
+
+
+async def _get_title_sku(product_uri, soup):
+    try:
+        title_sku = soup.find('h1',
+                              class_='sc-fubCzh ibFUIH product__title').getText(
+            strip=True).replace('\u00A0', '')
+        print(f'[+] Товар {title_sku}')
+    except:
+        title_sku = 'Не удалось прочитать наименование товара'
+        print(
+            f"Не удалось прочитать наименование товара {product_uri} в магазине перекрёсток")
+    return title_sku
