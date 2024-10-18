fd2d98de4a347ae92cc069f5e39d3dc06fb2d5de
Asus <asus@mail.ru>
2024-10-18 15:20:29 +0300

    Добавлен модуль work_with_json.py.

---
+++ work_with_json.py
@@ -0,0 +1,14 @@
+import json
+import datetime
+
+
+def _save_promotions_in_json(promotions_in_stores, name_store):
+    with open(f'promotions_in_stores_{name_store}_{datetime.date.today()}.json',
+              'w') as file:
+        json.dump(promotions_in_stores, file, indent=4, ensure_ascii=False)
+
+
+def _save_products_in_json(products_in_stores, name_store):
+    with open(f'products_in_stores_{name_store}_{datetime.date.today()}.json',
+              'w') as file:
+        json.dump(products_in_stores, file, indent=4, ensure_ascii=False)
