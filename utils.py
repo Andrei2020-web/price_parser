1f1b529e598f3a254c7720a34a25ba7b023e6c0c
Asus <asus@mail.ru>
2024-12-11 19:30:52 +0300

    Добавлен импорт webdriver.

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import asyncio
 import spar_parser
 import lenta_parser
 import perekrestok_parser
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
